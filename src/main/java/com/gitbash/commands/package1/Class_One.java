089b952acd7aca63cc63d7b85708ce1b742753a1
RukhsarBanu247 <banurukhsar247@gmail.com>
2025-03-08 12:39:21 +0530

    commit1

---
+++ src/main/java/com/gitbash/commands/package1/Class_One.java
@@ -0,0 +1,11 @@
+package com.gitbash.commands.package1;
+
+public class Class_One {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hi");
+	}
+
+}
